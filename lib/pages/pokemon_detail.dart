b927bf4850420a26e0d605fe30792af07ce23697
Diego Acuña <45692265+diegoaces@users.noreply.github.com>
2025-10-27 02:08:33 -0300

    feat: Enhance PokemonDetail page layout with improved UI elements and error handling

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:poke_app/colors.dart';
 import 'package:poke_app/models/pokemon.dart';
 import 'package:poke_app/widgets/custom_bottom_navigation.dart';
 
@@ -11,42 +13,129 @@ class PokemonDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            context.pop();
-          },
-        ),
-        title: Text(pokemon.nameCapitalizedFirstLetter()),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Pokemon #${pokemon.id}',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              pokemon.nameCapitalizedFirstLetter(),
-              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 32),
-            Image.network(
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png',
-              height: 200,
-              width: 200,
-              errorBuilder: (context, error, stackTrace) => const Icon(
-                Icons.broken_image,
-                size: 100,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Header con imagen
+              SizedBox(
+                height: 380,
+                child: Stack(
+                  children: [
+                    // Fondo decorativo con circulo recortado
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      child: ClipPath(
+                        clipper: CircleClipper(),
+                        child: Container(
+                          height: 280,
+                          width: double.infinity,
+                          color: grassPrimary,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: ShaderMask(
+                        shaderCallback: (Rect bounds) {
+                          return LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Colors.white,
+                              Colors.white.withAlpha(200),
+                              Colors.white.withAlpha(26),
+                            ],
+                          ).createShader(bounds);
+                        },
+                        blendMode: BlendMode.srcIn,
+                        child: SvgPicture.asset(
+                          'assets/svg/grass.svg',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.network(
+                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png',
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.contain,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(
+                            Icons.image_not_supported,
+                            size: 200,
+                            color: Colors.white,
+                          );
+                        },
+                      ),
+                    ),
+                    // Contenido
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          // Botones de arriba
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  context.pop();
+                                },
+                                child: const Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: const Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          // Imagen del Pokémon
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-      bottomNavigationBar: CustomBottomNavigation()
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
+}
+
+class CircleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    // Creates a path with an oval that fills the given size
+    path.addArc(
+      Rect.fromCircle(
+        center: Offset(size.width / 2, 0),
+        radius: size.width / 1.5,
+      ),
+      0,
+      2 * 3.14159265358979323846,
+    );
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
